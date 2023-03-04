09ca7d17cf6d90e25bd8458aeb2a8033e155c402
Dilan <dilan.gomez@uhispano.ac.cr>
2023-03-04 01:34:36 -0600

    Add columns for roles, save button and filter

--- src/admin/users/UserList.tsx
+++ src/admin/users/UserList.tsx
@@ -1,14 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Divider, Heading } from '@aws-amplify/ui-react';
 import { useNavigate } from 'react-router-dom';
 import {
   Box,
   Button,
+  Checkbox,
   Container,
   Header,
   Icon,
   SpaceBetween,
   Table,
+  TextFilter,
 } from '@cloudscape-design/components';
 import { ListCoursesQuery } from 'src/API';
 import { ErrorNotification } from 'src/container/DataManagement/ErrorNotification';
@@ -30,6 +32,8 @@ export const AdminUserList = () => {
     }
   }, [usersList, isUsersListLoading]);
 
+  const [filterText, setFilterText] = useState<string>('');
+
   return (
     <Layout title='Usuarios'>
       <Container>
@@ -49,6 +53,7 @@ export const AdminUserList = () => {
               id: 'name',
               header: 'Nombre',
               cell: (e) => e?.name,
+              sortingField: 'name',
             },
             {
               id: 'email',
@@ -59,6 +64,21 @@ export const AdminUserList = () => {
               id: 'phoneNumber',
               header: 'Número de Teléfono',
               cell: (e) => e?.phoneNumber,
+            },
+            {
+              id: 'isAdmin',
+              header: 'Admin',
+              cell: (e) => <Checkbox checked={true}></Checkbox>,
+            },
+            {
+              id: 'isProfessor',
+              header: 'Profesor',
+              cell: (e) => <Checkbox checked={false}></Checkbox>,
+            },
+            {
+              id: 'isStudent',
+              header: 'Estudiante',
+              cell: (e) => <Checkbox checked={false}></Checkbox>,
             },
           ]}
           items={usersList ?? []}
@@ -73,7 +93,17 @@ export const AdminUserList = () => {
             </Box>
           }
           header={<Header />}
+          filter={
+            <TextFilter
+              filteringPlaceholder='Busca usuarios'
+              filteringText={filterText}
+              onChange={(e) => setFilterText(e.detail.filteringText)}
+            />
+          }
         />
+        <div style={{ paddingTop: '1rem', textAlign: 'right' }}>
+          <Button>Guardar</Button>
+        </div>
       </Container>
     </Layout>
   );
