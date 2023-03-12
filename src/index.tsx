4bed10f7e835b1db22300746d6aa942c084236a7
Dilan <dilan.gomez@uhispano.ac.cr>
2023-03-12 15:12:37 -0600

    Add primereact

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
+// theme
+import 'primereact/resources/themes/lara-light-blue/theme.css';
+// core
+import 'primereact/resources/primereact.min.css';
+// icons
+import 'primeicons/primeicons.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './container/App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
